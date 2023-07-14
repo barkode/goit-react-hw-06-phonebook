1c220877070284e53c5dcb36882c6725aca2ddd4
barkode <konstantinks@gmail.com>
2023-07-15 00:28:39 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,79 +1,30 @@
-import { useEffect, useState } from 'react';
 import PropType from 'prop-types';
+import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
 import { Container } from './App.styled';
 
-const LS_KEY = 'contacts';
-
 export function App() {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const parseContacts = JSON.parse(localStorage.getItem(LS_KEY));
-    if (parseContacts) {
-      setContacts(parseContacts);
-    }
-  }, []);
-
-  useEffect(() => {
-    // contacts.length !== 0 &&
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleFilterChange = e => {
-    setFilter(e.target.value);
-  };
-
-  const handleAddContact = newContact => {
-    const arrayOfNames = contacts.map(contact => contact.name.toLowerCase());
-    const ifAbonentExist = arrayOfNames.find(
-      name => newContact.name.toLowerCase() === name
-    );
-    if (ifAbonentExist) {
-      alert(`${newContact.name} is already in contacts`);
-      return;
-    }
-    setContacts([...contacts, newContact]);
-    setFilter('');
-  };
-
-  const handleDeleteContact = id => {
-    setContacts(prev => {
-      return prev.filter(contact => contact.id !== id);
-    });
-  };
-
-  const handleFilterAbonent = () => {
-    return contacts.filter(({ name }) => {
-      const abonentName = name.toLowerCase();
-      const abonentFilter = filter.toLowerCase();
-      return abonentName.includes(abonentFilter);
-    });
-  };
-
   return (
     <Container>
       <h1>Phone book</h1>
-      <ContactForm onAddContact={handleAddContact} />
+      <ContactForm />
       <h2>Contacts</h2>
-      <Filter filter={filter} onFilterChange={handleFilterChange} />
-      <ContactList
-        contacts={filter ? handleFilterAbonent() : contacts}
-        handleDeleteContact={handleDeleteContact}
-      />
+      <Filter />
+      <ContactList />
     </Container>
   );
 }
 
+export default App;
+
 App.propTypes = {
   contacts: PropType.arrayOf(
     PropType.shape({
-      id: PropType.string.isRequired,
-      name: PropType.string.isRequired,
-      number: PropType.string.isRequired,
+      id: PropType.string,
+      name: PropType.string,
+      number: PropType.string,
     })
   ),
 };
